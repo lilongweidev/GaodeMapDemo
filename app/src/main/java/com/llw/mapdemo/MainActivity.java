5078e3ae1b486ca6f2a76a2e996be4aa613c53f1
llw <1317057860@qq.com>
2020-12-25 17:46:55 +0800

    高德地图Demo

---
+++ app/src/main/java/com/llw/mapdemo/MainActivity.java
@@ -0,0 +1,359 @@
+package com.llw.mapdemo;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.LocationSource;
+import com.amap.api.maps.MapView;
+import com.amap.api.maps.UiSettings;
+import com.amap.api.maps.model.BitmapDescriptorFactory;
+import com.amap.api.maps.model.MyLocationStyle;
+import com.amap.api.services.core.PoiItem;
+import com.amap.api.services.poisearch.PoiResult;
+import com.amap.api.services.poisearch.PoiSearch;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.EasyPermissions;
+
+/**
+ * 主页面
+ * @author llw
+ */
+public class MainActivity extends AppCompatActivity implements
+        AMapLocationListener, LocationSource, PoiSearch.OnPoiSearchListener {
+
+    //请求权限码
+    private static final int REQUEST_PERMISSIONS = 9527;
+
+    //声明AMapLocationClient类对象
+    public AMapLocationClient mLocationClient = null;
+    //声明AMapLocationClientOption对象
+    public AMapLocationClientOption mLocationOption = null;
+
+    private MapView mapView;
+
+    //地图控制器
+    private AMap aMap = null;
+    //位置更改监听
+    private OnLocationChangedListener mListener;
+    //定位样式
+    private MyLocationStyle myLocationStyle = new MyLocationStyle();
+
+    //定义一个UiSettings对象
+    private UiSettings mUiSettings;
+
+    //POI查询对象
+    private PoiSearch.Query query;
+    //POI搜索对象
+    private PoiSearch poiSearch;
+    //城市码
+    private String cityCode = null;
+    //浮动按钮
+    private FloatingActionButton fabPOI;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fabPOI = findViewById(R.id.fab_poi);
+
+        //初始化定位
+        initLocation();
+
+        //初始化地图
+        initMap(savedInstanceState);
+
+        //检查Android版本
+        checkingAndroidVersion();
+    }
+
+    /**
+     * 初始化定位
+     */
+    private void initLocation() {
+        //初始化定位
+        mLocationClient = new AMapLocationClient(getApplicationContext());
+        //设置定位回调监听
+        mLocationClient.setLocationListener(this);
+        //初始化AMapLocationClientOption对象
+        mLocationOption = new AMapLocationClientOption();
+        //设置定位模式为AMapLocationMode.Hight_Accuracy，高精度模式。
+        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+        //获取最近3s内精度最高的一次定位结果：
+        //设置setOnceLocationLatest(boolean b)接口为true，启动定位时SDK会返回最近3s内精度最高的一次定位结果。如果设置其为true，setOnceLocation(boolean b)接口也会被设置为true，反之不会，默认为false。
+        mLocationOption.setOnceLocationLatest(true);
+        //设置是否返回地址信息（默认返回地址信息）
+        mLocationOption.setNeedAddress(true);
+        //设置定位请求超时时间，单位是毫秒，默认30000毫秒，建议超时时间不要低于8000毫秒。
+        mLocationOption.setHttpTimeOut(20000);
+        //关闭缓存机制，高精度定位会产生缓存。
+        mLocationOption.setLocationCacheEnable(false);
+        //给定位客户端对象设置定位参数
+        mLocationClient.setLocationOption(mLocationOption);
+    }
+
+    /**
+     * 初始化地图
+     *
+     * @param savedInstanceState
+     */
+    private void initMap(Bundle savedInstanceState) {
+        mapView = findViewById(R.id.map_view);
+        //在activity执行onCreate时执行mMapView.onCreate(savedInstanceState)，创建地图
+        mapView.onCreate(savedInstanceState);
+        //初始化地图控制器对象
+        aMap = mapView.getMap();
+
+        //设置最小缩放等级为16 ，缩放级别范围为[3, 20]
+        aMap.setMinZoomLevel(16);
+        //开启室内地图
+        aMap.showIndoorMap(true);
+
+        //实例化UiSettings类对象
+        mUiSettings = aMap.getUiSettings();
+        //隐藏缩放按钮 默认显示
+        mUiSettings.setZoomControlsEnabled(false);
+        //显示比例尺 默认不显示
+        mUiSettings.setScaleControlsEnabled(true);
+
+        // 自定义定位蓝点图标
+        myLocationStyle.myLocationIcon(BitmapDescriptorFactory.fromResource(R.drawable.gps_point));
+        // 自定义精度范围的圆形边框颜色  都为0则透明
+        myLocationStyle.strokeColor(Color.argb(0, 0, 0, 0));
+        // 自定义精度范围的圆形边框宽度  0 无宽度
+        myLocationStyle.strokeWidth(0);
+        // 设置圆形的填充颜色  都为0则透明
+        myLocationStyle.radiusFillColor(Color.argb(0, 0, 0, 0));
+
+        //设置定位蓝点的Style
+        aMap.setMyLocationStyle(myLocationStyle);
+
+        // 设置定位监听
+        aMap.setLocationSource(this);
+        // 设置为true表示显示定位层并可触发定位，false表示隐藏定位层并不可触发定位，默认是false
+        aMap.setMyLocationEnabled(true);
+    }
+
+    /**
+     * 检查Android版本
+     */
+    private void checkingAndroidVersion() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            //Android6.0及以上先获取权限再定位
+            requestPermission();
+        } else {
+            //Android6.0以下直接定位
+            //启动定位
+            mLocationClient.startLocation();
+        }
+    }
+
+    /**
+     * 动态请求权限
+     */
+    @AfterPermissionGranted(REQUEST_PERMISSIONS)
+    private void requestPermission() {
+        String[] permissions = {
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.READ_PHONE_STATE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+        };
+
+        if (EasyPermissions.hasPermissions(this, permissions)) {
+            //true 有权限 开始定位
+            showMsg("已获得权限，可以定位啦！");
+
+            //启动定位
+            mLocationClient.startLocation();
+        } else {
+            //false 无权限
+            EasyPermissions.requestPermissions(this, "需要权限", REQUEST_PERMISSIONS, permissions);
+        }
+    }
+
+    /**
+     * 请求权限结果
+     *
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        //设置权限请求结果
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+    }
+
+    /**
+     * Toast提示
+     *
+     * @param msg 提示内容
+     */
+    private void showMsg(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * 接收异步返回的定位结果
+     *
+     * @param aMapLocation
+     */
+    @Override
+    public void onLocationChanged(AMapLocation aMapLocation) {
+        if (aMapLocation != null) {
+            if (aMapLocation.getErrorCode() == 0) {
+                //地址
+                String address = aMapLocation.getAddress();
+                //获取纬度
+                double latitude = aMapLocation.getLatitude();
+                //获取经度
+                double longitude = aMapLocation.getLongitude();
+
+                Log.d("MainActivity", aMapLocation.getCity());
+                showMsg(address);
+
+                //停止定位后，本地定位服务并不会被销毁
+                mLocationClient.stopLocation();
+
+                //显示地图定位结果
+                if (mListener != null) {
+                    // 显示系统图标
+                    mListener.onLocationChanged(aMapLocation);
+                }
+
+                //显示浮动按钮
+                fabPOI.show();
+                //赋值
+                cityCode = aMapLocation.getCityCode();
+            } else {
+                //定位失败时，可通过ErrCode（错误码）信息来确定失败的原因，errInfo是错误信息，详见错误码表。
+                Log.e("AmapError", "location Error, ErrCode:"
+                        + aMapLocation.getErrorCode() + ", errInfo:"
+                        + aMapLocation.getErrorInfo());
+            }
+        }
+    }
+
+    /**
+     * 激活定位
+     */
+    @Override
+    public void activate(OnLocationChangedListener onLocationChangedListener) {
+        mListener = onLocationChangedListener;
+        if (mLocationClient == null) {
+            mLocationClient.startLocation();//启动定位
+        }
+    }
+
+    /**
+     * 停止定位
+     */
+    @Override
+    public void deactivate() {
+        mListener = null;
+        if (mLocationClient != null) {
+            mLocationClient.stopLocation();
+            mLocationClient.onDestroy();
+        }
+        mLocationClient = null;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //销毁定位客户端，同时销毁本地定位服务。
+        if (mLocationClient != null) {
+            mLocationClient.onDestroy();
+        }
+        //在activity执行onDestroy时执行mMapView.onDestroy()，销毁地图
+        mapView.onDestroy();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView.onResume ()，重新绘制加载地图
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView.onPause ()，暂停地图的绘制
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        //在activity执行onSaveInstanceState时执行mMapView.onSaveInstanceState (outState)，保存地图当前的状态
+        mapView.onSaveInstanceState(outState);
+    }
+
+    /**
+     * POI搜索返回
+     *
+     * @param poiResult POI所有数据
+     * @param i
+     */
+    @Override
+    public void onPoiSearched(PoiResult poiResult, int i) {
+        //解析result获取POI信息
+
+        //获取POI组数列表
+        ArrayList<PoiItem> poiItems = poiResult.getPois();
+        for (PoiItem poiItem : poiItems) {
+            Log.d("MainActivity", " Title：" + poiItem.getTitle() + " Snippet：" + poiItem.getSnippet());
+        }
+    }
+
+    /**
+     * POI中的项目搜索返回
+     *
+     * @param poiItem 获取POI item
+     * @param i
+     */
+    @Override
+    public void onPoiItemSearched(PoiItem poiItem, int i) {
+
+    }
+
+    /**
+     * 浮动按钮点击查询附近POI
+     *
+     * @param view
+     */
+    public void queryPOI(View view) {
+        //构造query对象
+        query = new PoiSearch.Query("购物", "", cityCode);
+        // 设置每页最多返回多少条poiitem
+        query.setPageSize(10);
+        //设置查询页码
+        query.setPageNum(1);
+        //构造 PoiSearch 对象
+        poiSearch = new PoiSearch(this, query);
+        //设置搜索回调监听
+        poiSearch.setOnPoiSearchListener(this);
+        //发起搜索附近POI异步请求
+        poiSearch.searchPOIAsyn();
+    }
+}
